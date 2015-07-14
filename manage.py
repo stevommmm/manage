9e26e6a3cb3abaff8f7c89ea2667441167581a92
stevommmm <git@c45y.com>
2015-07-15 09:48:39 +1000

    shift sorting into a contained class

--- manage.py
+++ manage.py
@@ -24,8 +24,12 @@ from pprint import pprint as pp
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
-UNSORTED = os.path.join(ROOT, 'unsorted')
-PLUGINS = os.path.join(ROOT, 'plugins')
+
+def _safejoin(*args):
+	"""Ensure we're not open to directory traversal"""
+	p = os.path.abspath(os.path.join(*args))
+	assert p.startswith(ROOT)
+	return p
 
 def _confirmwrite(jarloc):
 	print("Please confirm overwrite file with different hash at location: %s" % jarloc)
@@ -33,12 +37,6 @@ def _confirmwrite(jarloc):
 		n = raw_input('Overwrite file? [y/n] ')
 		if n in ['y', 'n']:
 			return n == 'y'
-
-def _safejoin(*args):
-	"""Ensure we're not open to directory traversal"""
-	p = os.path.abspath(os.path.join(*args))
-	assert p.startswith(ROOT)
-	return p
 
 def _comparefiles(unsortedjar, newjar):
 	"""Compare two files using their hash, low chance of md5 collision"""
@@ -51,62 +49,66 @@ def _makedir(*args):
 		os.mkdir(_safejoin(*args))
 		logging.info('Folder created (%s)', os.path.join(*args))
 
-def get_plugins():
-	"""Enumerate our unsorted plugin folder, return .jar files"""
-	for f in os.listdir(UNSORTED):
-		if f.endswith('.jar'):
-			yield f
 
-def get_plugin(filepath):
-	"""Return an open file handle to our .jar file"""
-	return zipfile.ZipFile(_safejoin(UNSORTED, filepath), 'r')
+class pluginSorter(object):
+	""" Sort plugins based on plugin.yml stated version """
+	def __init__(self, options):
+		self.plugin_dir = _safejoin(ROOT, 'plugins')
+		self.unsorted_dir = _safejoin(ROOT, 'unsorted')
 
-def get_pluginyaml(filepath):
-	"""Rip the plugin.yml out of our plugin jar, return a python object"""
-	with get_plugin(filepath) as _zip:
-		return yaml.load(_zip.read('plugin.yml'))
+		if not options['--folder'] is None:
+			self.unsorted_dir = os.path.join(ROOT, options['--folder'])
 
-def get_plugin_info(pluginyaml):
-	"""Filter the yaml returned into plugin name and version"""
-	return map(str, [pluginyaml['name'], pluginyaml['version']])
+		_makedir(self.unsorted_dir)
+		_makedir(self.plugin_dir)
 
-def move_version(pluginname, version, filename):
-	"""Move an unsorted jar to its final location, ensure that we
-	   have created all the directories in the path
-	"""
-	_makedir(PLUGINS, pluginname)
-	_makedir(PLUGINS, pluginname, version.replace(os.sep, '_'))
-	pfile = _safejoin(PLUGINS, pluginname, version, pluginname.lower() + ".jar")
-	sfile = _safejoin(UNSORTED, filename)
-	if os.path.exists(pfile) and not _comparefiles(sfile, pfile):
-		if not _confirmwrite(pfile):
-			return
-	
-	with open(pfile, 'wb+') as ouf:
-		with open(sfile, 'rb') as inf:
-			ouf.write(inf.read())
-			logging.info('Placed (%s) to (%s)', filename, pfile)
-	os.remove(_safejoin(UNSORTED, filename))
+		for x in self.get_plugins():
+			plugin, version = self.get_plugin_info(self.get_pluginyaml(x))
+			self.move_version(plugin, version, x)
 
-def sort_main(args):
-	"""Handler for the sort command"""
-	if not args['--folder'] is None:
-		global UNSORTED
-		UNSORTED = os.path.join(ROOT, args['--folder'])
+	def get_plugins(self):
+		"""Enumerate our unsorted plugin folder, return .jar files"""
+		for f in os.listdir(self.unsorted_dir):
+			if f.endswith('.jar'):
+				yield f
 
-	_makedir(UNSORTED)
-	_makedir(PLUGINS)
+	def get_plugin(self, filepath):
+		"""Return an open file handle to our .jar file.
+		   Use from a with block to esure file handle close
+		"""
+		return zipfile.ZipFile(_safejoin(self.unsorted_dir, filepath), 'r')
 
-	for x in get_plugins():
-		plugin, version = get_plugin_info(get_pluginyaml(x))
-		move_version(plugin, version, x)
-		x.close()
+	def get_pluginyaml(self, filepath):
+		"""Rip the plugin.yml out of our plugin jar, return a python object"""
+		with self.get_plugin(filepath) as _zip:
+			return yaml.load(_zip.read('plugin.yml'))
 
+	def get_plugin_info(self, pluginyaml):
+		"""Filter the yaml returned into plugin name and version"""
+		return map(str, [pluginyaml['name'], pluginyaml['version']])
+
+	def move_version(self, pluginname, version, filename):
+		"""Move an unsorted jar to its final location, ensure that we
+		   have created all the directories in the path
+		"""
+		_makedir(self.plugin_dir, pluginname)
+		_makedir(self.plugin_dir, pluginname, version.replace(os.sep, '_'))
+		pfile = _safejoin(self.plugin_dir, pluginname, version, pluginname.lower() + ".jar")
+		sfile = _safejoin(self.unsorted_dir, filename)
+		if os.path.exists(pfile) and not _comparefiles(sfile, pfile):
+			if not _confirmwrite(pfile):
+				return
+		
+		with open(pfile, 'wb+') as ouf:
+			with open(sfile, 'rb') as inf:
+				ouf.write(inf.read())
+				logging.info('Placed (%s) to (%s)', filename, pfile)
+		os.remove(_safejoin(self.unsorted_dir, filename))
 
 if __name__ == '__main__':
 	arguments = docopt(__doc__, version='Manage.py 1.0')
 
 	if arguments['sort']:
-		sort_main(arguments)
+		pluginSorter(arguments)
 
 	
